397c78694f2e57d149fbf0682bf47e7be5bdc2e5
Nikolay Ashroev <kolya-200587@mail.ru>
2026-02-13 08:53:26 -0500

    Add files via upload

--- esphome/components/epaper_spi/epaper_waveshare_4color.cpp
+++ esphome/components/epaper_spi/epaper_waveshare_4color.cpp
@@ -12,8 +12,6 @@ static const char *const TAG = "epaper_spi.waveshare_4color";
 
 uint8_t EpaperWaveshare4Color::get_color_bits_(Color color) {
   // Convert ESPHome Color to 2-bit display color
-  // For 4-color displays, we need to detect red and yellow
-  
   uint8_t r = color.r;
   uint8_t g = color.g;
   uint8_t b = color.b;
@@ -37,14 +35,14 @@ uint8_t EpaperWaveshare4Color::get_color_bits_(Color color) {
   return 0b00;  // Black
 }
 
-void EpaperWaveshare4Color::draw_pixel(int16_t x, int16_t y, Color color) {
-  if (x < 0 || x >= this->get_width() || y < 0 || y >= this->get_height()) {
+void EpaperWaveshare4Color::draw_absolute_pixel_internal(int x, int y, Color color) {
+  if (x < 0 || x >= this->get_width_internal() || y < 0 || y >= this->get_height_internal()) {
     return;
   }
   
   // Calculate buffer position
   // Each byte contains 4 pixels (2 bits per pixel)
-  uint32_t pixel_index = x + y * this->get_width();
+  uint32_t pixel_index = x + y * this->get_width_internal();
   uint32_t byte_index = pixel_index / 4;
   uint8_t bit_position = (pixel_index % 4) * 2;
   
@@ -67,94 +65,35 @@ void EpaperWaveshare4Color::draw_pixel(int16_t x, int16_t y, Color color) {
 }
 
 bool EpaperWaveshare4Color::initialise(bool partial) {
-  EPaperBase::initialise(partial);
+  // Call parent initialization
+  EPaperMono::initialise(partial);
   
   // 4-color displays don't support partial refresh
   if (partial) {
     ESP_LOGW(TAG, "Partial refresh not supported on 4-color displays, using full refresh");
   }
   
-  // Wait for the display to be ready after init sequence
-  this->next_delay_ = 10;
-  this->send_red_ = false;
+  // Send LUT (even though 4-color displays typically don't use it in the same way,
+  // we keep the API compatible with the base class)
+  if (partial && this->partial_lut_ != nullptr) {
+    this->cmd_data(0x32, this->partial_lut_, this->partial_lut_length_);
+  } else if (this->lut_ != nullptr) {
+    this->cmd_data(0x32, this->lut_, this->lut_length_);
+  }
   
   return true;
 }
 
-void EpaperWaveshare4Color::set_window() {
-  // Set RAM X address (horizontal)
-  // Address is in bytes (width/8), but for 4-color we need width/4
-  uint8_t x_start = this->x_low_ / 4;
-  uint8_t x_end = (this->x_high_ + 3) / 4 - 1;
+void EpaperWaveshare4Color::init_buffer_(size_t length) {
+  // For 4-color display, we need 2 bits per pixel instead of 1
+  // So buffer size is (width * height) / 4 instead of (width * height) / 8
+  size_t buffer_length = (this->get_width_internal() * this->get_height_internal() + 3) / 4;
   
-  this->cmd_data(0x44, {x_start, x_end});  // Set RAM X address start/end
-  this->cmd_data(0x4E, {x_start});         // Set RAM X address counter
+  // Call parent's init_buffer_ with the correct size
+  EPaperMono::init_buffer_(buffer_length);
   
-  // Set RAM Y address (vertical) 
-  uint16_t y_start = this->y_low_;
-  uint16_t y_end = this->y_high_ - 1;
-  
-  this->cmd_data(0x45, {
-    (uint8_t)(y_start & 0xFF),
-    (uint8_t)(y_start >> 8),
-    (uint8_t)(y_end & 0xFF),
-    (uint8_t)(y_end >> 8)
-  });
-  this->cmd_data(0x4F, {
-    (uint8_t)(y_start & 0xFF),
-    (uint8_t)(y_start >> 8)
-  });
-  
-  ESP_LOGV(TAG, "Set window X: %u-%u, Y: %u-%u", this->x_low_, this->x_high_, 
-           this->y_low_, this->y_high_);
-}
-
-void EpaperWaveshare4Color::write_init_buffer_() {
-  // For 4-color display, write initial buffer as all white
-  this->command(0x24);  // Write RAM command
-  
-  uint32_t buffer_size = (this->get_width() * this->get_height() + 3) / 4;
-  for (uint32_t i = 0; i < buffer_size; i++) {
-    this->data(0x55);  // 0x55 = 01010101 = all white pixels
-  }
-}
-
-void EpaperWaveshare4Color::write_buffer_() {
-  // Write the image buffer to display RAM
-  this->set_window();
-  
-  this->command(0x24);  // Write RAM command
-  
-  // Calculate the buffer size based on dirty region
-  uint32_t width_bytes = (this->x_high_ - this->x_low_ + 3) / 4;
-  uint32_t buffer_width_bytes = (this->get_width() + 3) / 4;
-  
-  for (uint16_t y = this->y_low_; y < this->y_high_; y++) {
-    uint32_t start_index = (y * buffer_width_bytes) + (this->x_low_ / 4);
-    this->data_multi(this->buffer_ + start_index, width_bytes);
-  }
-  
-  ESP_LOGV(TAG, "Wrote buffer, region: X=%u-%u, Y=%u-%u", 
-           this->x_low_, this->x_high_, this->y_low_, this->y_high_);
-}
-
-void EpaperWaveshare4Color::refresh_screen(bool partial) {
-  // 4-color displays only support full refresh
-  this->cmd_data(0x22, {0xF7});  // Display Update Control 2
-  this->command(0x20);            // Master Activation
-  
-  // Wait for display to refresh (4-color displays are slower)
-  this->next_delay_ = 15000;  // 15 seconds for full refresh
-  
-  ESP_LOGD(TAG, "Display refresh initiated");
-}
-
-void EpaperWaveshare4Color::deep_sleep() {
-  // Enter deep sleep mode
-  this->cmd_data(0x10, {0x01});
-  this->next_delay_ = 100;
-  
-  ESP_LOGD(TAG, "Entering deep sleep mode");
+  ESP_LOGD(TAG, "Initialized 4-color buffer: %u bytes for %dx%d display",
+           buffer_length, this->get_width_internal(), this->get_height_internal());
 }
 
 }  // namespace esphome::epaper_spi
--- esphome/components/epaper_spi/epaper_waveshare_4color.h
+++ esphome/components/epaper_spi/epaper_waveshare_4color.h
@@ -1,35 +1,48 @@
 #pragma once
 #include "epaper_spi.h"
+#include "epaper_spi_mono.h"
 
 namespace esphome::epaper_spi {
 
 /**
  * An epaper display for Waveshare 4-color (BWRY) displays.
+ * 
  * These displays use 2 bits per pixel to represent 4 colors:
  * - Black (00)
  * - White (01)  
  * - Red (10)
  * - Yellow (11)
+ * 
+ * Based on EPaperMono but with 4-color buffer handling.
  */
-class EpaperWaveshare4Color final : public EPaperBase {
+class EpaperWaveshare4Color final : public EPaperMono {
  public:
   EpaperWaveshare4Color(const char *name, uint16_t width, uint16_t height, 
-                        const uint8_t *init_sequence, size_t init_sequence_length)
-      : EPaperBase(name, width, height, init_sequence, init_sequence_length) {}
+                        const uint8_t *init_sequence, size_t init_sequence_length,
+                        const uint8_t *lut, size_t lut_length, 
+                        const uint8_t *partial_lut, uint16_t partial_lut_length)
+      : EPaperMono(name, width, height, init_sequence, init_sequence_length),
+        lut_(lut),
+        lut_length_(lut_length),
+        partial_lut_(partial_lut),
+        partial_lut_length_(partial_lut_length) {}
 
-  void draw_pixel(int16_t x, int16_t y, Color color) override;
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
+  int get_width_internal() override { return this->width_; }
+  int get_height_internal() override { return this->height_; }
   
  protected:
   bool initialise(bool partial) override;
-  void set_window() override;
-  void refresh_screen(bool partial) override;
-  void deep_sleep() override;
-  void write_init_buffer_() override;
-  void write_buffer_() override;
+  void init_buffer_(size_t length) override;
   
  private:
   // Helper to get 2-bit color value from Color object
   uint8_t get_color_bits_(Color color);
+  
+  const uint8_t *lut_;
+  size_t lut_length_;
+  const uint8_t *partial_lut_;
+  uint16_t partial_lut_length_;
 };
 
 }  // namespace esphome::epaper_spi

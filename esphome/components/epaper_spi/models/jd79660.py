d52f073eb72c715257470bb091b306d2cd8d4c67
Froyo <kolya-200587@mail.ru>
2026-02-15 19:31:46 -0500

    try one more thing

--- esphome/components/epaper_spi/models/jd79660.py
+++ esphome/components/epaper_spi/models/jd79660.py
@@ -7,6 +7,17 @@ from esphome.core import ID
 from ..display import CONF_INIT_SEQUENCE_ID
 from . import EpaperModel
 
+# Waveshare 2.13-G V2
+jd79660.extend(
+    "Waveshare-2.13in-G-V23",
+    width=122,
+    height=250,
+    initsequence=(
+        (0x61, 0x00, 0x7A, 0x00, 0xFA,),  # Resolution: 122x250
+        (0xE9, 0x01,),                     # Enable features
+        (0x04,),                           # Power on
+    ),
+)
 
 class JD79660(EpaperModel):
     def __init__(self, name, class_name="EPaperJD79660", fast_update=None, **kwargs):
